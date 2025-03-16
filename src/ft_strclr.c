644d5464399ddcf8a3e6ec0799dce413eff94eec
Lilian Monsat <lmonsat@c2r4s3.42luxembourg.lu>
2025-03-16 13:31:59 +0100

    norme

--- src/ft_strclr.c
+++ src/ft_strclr.c
@@ -12,31 +12,6 @@
 
 #include "../include/libft.h"
 
-size_t	ft_strlen(const char *s)
-{
-	size_t	i;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
-void	*ft_memset(void *s, int c, size_t n)
-{
-	unsigned char	*p;
-
-	p = s;
-	while (n != 0)
-	{
-		*p++ = (unsigned char)c;
-		n--;
-	}
-	return (s);
-}
-
 void	ft_strclr(char *s)
 {
 	size_t	size;
